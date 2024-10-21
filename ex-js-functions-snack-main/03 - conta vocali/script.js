9eb6043aaee924425792b927372202854bb57588
Carlo Elvisi <carloelvisi01@outlook.it>
2024-10-21 16:07:40 +0200

    es 3

--- ex-js-functions-snack-main/03 - conta vocali/script.js
+++ ex-js-functions-snack-main/03 - conta vocali/script.js
@@ -1,13 +1,19 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
-
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
+const onlyVocali = (word) => {
+  const vocali = ["a", "e", "i", "o", "u"]; 
+  let countVocali = 0; 
 
+  for (let i = 0; i < word.length; i++) {
+    if (vocali.includes(word[i])) {
+      
+      countVocali++; 
+    }
+  }
+  return countVocali; 
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
-//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+const onlyVocaliCount = onlyVocali(word);
+console.log(onlyVocaliCount); // Risultato atteso: 3 (a, a, i)
