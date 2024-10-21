37d88c5e0377cd08a12f93c5b4e857d18a866d4a
Carlo Elvisi <carloelvisi01@outlook.it>
2024-10-21 16:11:37 +0200

    es 4

--- ex-js-functions-snack-main/04 - saluti/script.js
+++ ex-js-functions-snack-main/04 - saluti/script.js
@@ -1,13 +1,12 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-
+const addCiao = (name) => `Ciao ${name}`;
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const addSaluto = addCiao(name);
+console.log(addSaluto);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
